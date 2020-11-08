18bba7045583594d2c5c3230169b2634b2fccf0b
NataliaMikhaylova <natasha.mikhaylova17052002@mail.ru>
2020-11-08 22:35:39 +0300

    № 3

---
+++ Individual_3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Вариант 14
+# Вычислить сумму всех n-значных чисел (1 <= n <=4).
+
+if __name__ == '__main__':
+    n = 0
+    for i in range(0, 5):
+        n += i
+    print(n)
